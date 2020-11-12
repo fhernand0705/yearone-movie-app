ff4128d23952921b737b1acba861c5ea89409262
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-11-12 14:07:04 -0500

    started movie app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app without errors', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = screen.getByTestId("app-component");
+  expect(app).toBeInTheDocument();
 });
